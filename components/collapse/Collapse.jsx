e6265ba5f385dd3e374b932dc05708a350d4d529
freeman <zhhyang0404@163.com>
2017-05-06 14:45:09 +0800

    折叠面板

---
+++ components/collapse/Collapse.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+export default class Collapse extends Component {
+
+    render() {
+        return (
+
+        )
+    }
+}
+Collapse.propTypes = {}
+Collapse.defaultProps = {}
---
+++ components/collapse/index.jsx
@@ -0,0 +1,3 @@
+/**
+ * Created by freeman on 17-5-6.
+ */
