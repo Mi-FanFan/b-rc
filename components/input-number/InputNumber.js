c560dc25e35e533fa436662e84bf7643f848ae28
zhhyang <zhhyang0404@gmail.com>
2017-06-17 16:27:58 +0800

    bugfix:数字输入框添加readOnly参数，以及输入时是否保留小数，precision=0则不保留，precision ！= 0 则按step保留位数

--- components/input-number/InputNumber.js
+++ components/input-number/InputNumber.js
@@ -19,154 +19,155 @@ export default class InputNumber extends Component {
     this.handleBlur = this.handleBlur.bind(this)
     this.state = {
       isFocused: false,
-      value:props.defaultValue
+      value: props.defaultValue
     }
   }
 
-  handleDec() {
-    this.dec();
+  handleDec () {
+    this.dec()
   }
 
-  handleInc() {
-    this.inc();
+  handleInc () {
+    this.inc()
   }
 
-  toFixed(value){
-    const old = this.getPrecision(this.state.value);
-    const step = this.getPrecision(this.props.step);
+  toFixed (value) {
+    const old = this.getPrecision(this.state.value)
+    const step = this.getPrecision(this.props.step)
     if (!old && !step) {
-      return value;
+      return value
     }
-    return value.toFixed(Math.max(old, step));
+    return value.toFixed(Math.max(old, step))
   }
 
-  getPrecision(value){
-    const v = String(value);
+  getPrecision (value) {
+    const v = String(value)
     if (!/\./.test(v)) {
-      return 0;
+      return 0
     }
-    return v.split('.')[1].length;
+    return v.split('.')[1].length
   }
 
-  handleChange(e) {
-    this.updateValue(e.target.value);
+  handleChange (e) {
+    this.updateValue(e.target.value)
   }
 
-  updateValue(v){
-    const {disabled ,onChange,min,max} = this.props
-    if (disabled || !onChange){
+  updateValue (v) {
+    const {disabled, onChange, min, max, precision} = this.props
+    if (disabled || !onChange) {
       return
     }
     v = parseFloat(v)
-    if (isNaN(v)){
+    if (isNaN(v)) {
       return
     }
-    if (min !== undefined && v <min){
+    if (min !== undefined && v < min) {
       v = min
     }
-    if (max !== undefined && v > max){
+    if (max !== undefined && v > max) {
       v = max
     }
-    v = parseFloat(this.toFixed(v))
+    v = precision > 0 ? parseFloat(this.toFixed(v)) : parseInt(this.toFixed(v))
     this.setState({
-      value:v
+      value: v
     })
     onChange(v)
   }
 
-  checkDecAvailable() {
-    const {disabled,min} = this.props
+  checkDecAvailable () {
+    const {disabled, min} = this.props
     const {value} = this.state
     if (disabled) {
-      return false;
+      return false
     }
 
     if (!Number.isInteger(min)) {
-      return true;
+      return true
     }
 
-    return value > min;
+    return value > min
   }
 
-  checkIncAvailable() {
-    const {disabled,max} = this.props
+  checkIncAvailable () {
+    const {disabled, max} = this.props
     const {value} = this.state
     if (disabled) {
-      return false;
+      return false
     }
 
     if (!Number.isInteger(max)) {
-      return true;
+      return true
     }
 
-    return value < max;
+    return value < max
   }
-  dec() {
-    const {step,min} = this.props
+
+  dec () {
+    const {step, min} = this.props
     const {value} = this.state
-    let v = value - step;
+    let v = value - step
 
     if (v < min) {
-      v = min;
+      v = min
     }
 
-    this.updateValue(v);
+    this.updateValue(v)
   }
 
-  inc() {
-    const {step,max} = this.props
+  inc () {
+    const {step, max} = this.props
     const {value} = this.state
-    let v = value + step;
+    let v = value + step
 
     if (v > max) {
-      v = max;
+      v = max
     }
 
-    this.updateValue(v);
+    this.updateValue(v)
   }
 
-  handleKeyDown(e) {
-    const {min,max,step} = this.props
+  handleKeyDown (e) {
+    const {min, max, step} = this.props
     const {value} = this.state
     if (e.key === 'ArrowDown') {
-      this.dec();
+      this.dec()
     } else if (e.key === 'ArrowUp') {
-      this.inc();
+      this.inc()
     } else if (e.key === 'End' && min !== undefined) {
-      this.updateValue(min);
+      this.updateValue(min)
     } else if (e.key === 'Home' && max !== undefined) {
-      this.updateValue(max);
+      this.updateValue(max)
     } else if (e.key === 'PageDown') {
-      this.updateValue(value - (step * 10));
+      this.updateValue(value - (step * 10))
     } else if (e.key === 'PageUp') {
-      this.updateValue(value + (step * 10));
+      this.updateValue(value + (step * 10))
     }
   }
 
-  handleWheel(e) {
+  handleWheel (e) {
     if (!this.state.isFocused) {
-      return;
+      return
     }
 
-    e.preventDefault();
+    e.preventDefault()
 
     if (e.deltaY > 0) {
-      this.dec();
+      this.dec()
     } else {
-      this.inc();
+      this.inc()
     }
   }
 
-  handleFocus() {
-    this.setState({isFocused: true});
+  handleFocus () {
+    this.setState({isFocused: true})
   }
 
-  handleBlur() {
-    this.setState({isFocused: false});
+  handleBlur () {
+    this.setState({isFocused: false})
   }
 
   render () {
-    const {className, size, prefixCls, min, max, disabled,name,} = this.props
+    const {className, size, prefixCls, min, max, disabled, name, readOnly} = this.props
     const inputNumberClass = classNames({
       [`${prefixCls}`]: true,
       [`${prefixCls}-lg`]: size === 'large',
@@ -196,6 +197,7 @@ export default class InputNumber extends Component {
             autoComplete="off"
             disabled={disabled}
             name={name}
+            readOnly={readOnly}
             onChange={this.handleChange}
             onKeyDown={this.handleKeyDown}
             onWheel={this.handleWheel}
@@ -216,8 +218,10 @@ export default class InputNumber extends Component {
 InputNumber.defaultProps = {
   prefixCls: 'mff-input-number',
   disabled: false,
-  step:1,
-  onChange:()=>{},
+  step: 1,
+  readOnly: false,
+  precision: 0,
+  onChange: () => {},
 }
 InputNumber.propTypes = {
   prefixCls: PropTypes.string,
@@ -229,5 +233,7 @@ InputNumber.propTypes = {
   step: PropTypes.number,
   className: PropTypes.string,
   name: PropTypes.string,
+  readOnly: PropTypes.bool,
+  precision: PropTypes.number,
   onChange: PropTypes.func,
 }
